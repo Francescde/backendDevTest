49a2eab7352e0ac21d7730c32844d54270549e53
francesc.depuig <Vihey-50>
2023-06-06 09:01:10 +0200

    qucikfix timeout

--- myApp/myApp/src/main/java/com/example/myapp/config/RestTemplateConfig.java
+++ myApp/myApp/src/main/java/com/example/myapp/config/RestTemplateConfig.java
@@ -1,15 +1,28 @@
 package com.example.myapp.config;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class RestTemplateConfig {
 
+    @Value("${rest.template.connection-timeout}")
+    private int connectionTimeout;
+
+    @Value("${rest.template.read-timeout}")
+    private int readTimeout;
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder)
+    {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(connectionTimeout))
+           .setReadTimeout(Duration.ofSeconds(readTimeout))
+           .build();
     }
 
 }
